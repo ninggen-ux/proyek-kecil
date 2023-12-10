0854fa9ef4f4c341844709a6f82eaded2b6e2e11
Pemerintah <tangkawarowandika@gmail.com>
2023-12-09 19:55:46 -0800

    update

--- web-anime/script.js
+++ web-anime/script.js
@@ -2,14 +2,20 @@ const containerItem = document.querySelectorAll(".container__item");
 const containerItemKeterangan = document.querySelectorAll(".container__item__keterangan");
 const containerItemVideo = document.querySelectorAll(".container__item__video");
 
+document.addEventListener("DOMContentLoaded", () => {
+    containerItem.forEach(item => {
+        item.style.animationName = "animasi-awal";
+    })
+})
+
 containerItem.forEach((item, index) => {
-    item.addEventListener("mouseover", () => {
+    item.addEventListener("mouseenter", () => {
         containerItemKeterangan[index].style.opacity = 1;
         containerItemVideo[index].style.filter = `brightness(${100}%)`;
         containerItemVideo[index].play();
     })
 
-    item.addEventListener("mouseout", () => {
+    item.addEventListener("mouseleave", () => {
         containerItemKeterangan[index].style.opacity = 0;
         containerItemVideo[index].style.filter = `brightness(${50}%)`;
         containerItemVideo[index].pause();
