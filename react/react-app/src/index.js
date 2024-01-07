796d8d6c4b586eb9902c41944f52fc28340280af
Pemerintah <tangkawarowandika@gmail.com>
2024-01-06 18:52:13 -0800

    update

--- react/react-app/src/index.js
+++ react/react-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const element = <h1>Hello World!!!</h1>
+let hello = <h1>Hello World!!!</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render(hello);
