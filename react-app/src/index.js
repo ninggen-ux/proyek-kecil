ab992d63e06b5a598fd7a4579548db111354229b
Pemerintah <tangkawarowandika@gmail.com>
2024-01-12 04:53:25 -0800

    update

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+const batu = {
+    nama: "Batu",
+    tinggi: "tinggi 60cm",
+    beratBadan: "berat badan 652kg"
+}
 
-let x = (5 + 5) * 10;
-const contoh = <h1>{x > 10 ? "Lebih Besar" : "Lebih Kecil"}</h1>;
+let Gaje = (props) => {
+    return <h2>Nama saya {props.batu.nama} dan saya memiliki {props.batu.beratBadan}!</h2>;
+}
+
+let GajeDua = () => {
+    return (
+        <>
+            <h1>Hallo!!!!!!!!!!</h1>
+            <Gaje batu={batu} />
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(contoh);
+root.render(<GajeDua/>);
