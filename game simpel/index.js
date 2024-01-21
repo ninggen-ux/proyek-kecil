ed6a4e2c95deede10315c74614c3de0a2eef7f6d
Pemerintah <tangkawarowandika@gmail.com>
2024-01-21 03:52:17 -0800

    update

---
+++ game simpel/index.js
@@ -0,0 +1,11 @@
+const karakter = document.querySelector(".karakter");
+let left = 1;
+
+document.addEventListener("keydown", event => {
+    if (event.key === "a") {
+        karakter.style.left = `${left-- * 7}px`;
+        
+    } else if (event.key === "d") {
+        karakter.style.left = `${left++ * 7}px`;
+    }
+})
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-let Tulisan = (props) => {
-    return <ul><li>Saya adalah {props.buah}</li></ul>
-}
-
-let Buah = () => {
-    let jenisBuah = ["Mangga", "Kiwi", "pisang"];
+let Masukkan = () => {
     return (
-        <>
-            <h1>Buah!!!!!!!!!!!!!!!!!!!</h1>
-            {jenisBuah.map((item) => <Tulisan buah={item} />)}
-        </>
+        <form>
+            <lebel>Masukkan Nama Anda:</lebel>
+            <input type="text"></input>
+        </form>
     );
 }
 
-
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Buah/>);
+root.render(<Masukkan/>);
